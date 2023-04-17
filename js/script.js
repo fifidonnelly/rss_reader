827b5dc474ac146deae173911ec775502c496ae3
fifidonnelly <126438786+fifidonnelly@users.noreply.github.com>
2023-04-17 23:31:25 +0800

    took out more comments

--- js/script.js
+++ js/script.js
@@ -21,21 +21,13 @@ function addRSStoDOM(data) {
   content.appendChild(itemsContainer);
 }
 
-// The following gets the ADD RSS button to work.  This is a very similar
-// process that we did in the ToDo application.  Find the elements in
-// the HTML, then write a function to handle the element/event, then add
-// the event listener.
 let addFeedButton = document.getElementById("add-feed");
 let newRSSInput = document.getElementById("rss-input");
 
-/* Every time we add a task, save the task to local storage */
 function onAddRSSClicked(event) {
   let URL = newRSSInput.value;
   newRSSInput.value = "";
 
-  // Create and send a GET request
-  // The first argument is the post type (GET, POST, PUT, DELETE, etc.)
-  // The second argument is the endpoint URL
   xhr.open('GET', 'https://api.rss2json.com/v1/api.json?rss_url=' + URL);
   xhr.send();
 }

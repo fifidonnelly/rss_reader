95e32591357fe9dad351f72ef8c0448903aff652
fifidonnelly <126438786+fifidonnelly@users.noreply.github.com>
2023-04-18 21:47:48 +0800

    amended js

--- js/script.js
+++ js/script.js
@@ -28,7 +28,8 @@ var xhr = new XMLHttpRequest();
 xhr.onload = function() {
   if (xhr.status >=200 && xhr.status < 300) {  
     json = JSON.parse(xhr.responseText)
-    console.log("Success!")
+    console.log(json)
+    addRSStoDOM(json)
   } else { 
     console.log("The request failed")
     content.innerHTML = "The request failed, please check your RSS url"
